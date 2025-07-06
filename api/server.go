138f92abc3bb072a8646b706f3721d553eb49f08
Drolfothesgnir <kyryl.yeletsky@gmail.com>
2025-07-06 08:39:27 +0000

    added custom currency validator for api

--- api/server.go
+++ api/server.go
@@ -3,6 +3,8 @@ package api
 import (
 	db "github.com/Drolfothesgnir/simplebank/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 type Server struct {
@@ -14,16 +16,25 @@ func NewServer(store db.Store) *Server {
 	server := Server{store: store}
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", isValidCurency)
+	}
+
+	// accounts
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
 	router.PATCH("/accounts/:id", server.addAccountBalance)
 	router.DELETE("/accounts/:id", server.deleteAccount)
 
+	// transfers
+	router.POST("/transfers", server.createTransfer)
+
 	server.router = router
 	return &server
 }
 
+// Start runs the HTTP server on a specific address.
 func (server *Server) Start(address string) error {
 	return server.router.Run(address)
 }
