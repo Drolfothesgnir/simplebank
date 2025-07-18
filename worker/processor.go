e89a0968f76b8e6862b8903b991409720c72f8fd
Drolfothesgnir <kyryl.yeletsky@gmail.com>
2025-07-18 09:57:03 +0000

    added custom redis logger

--- worker/processor.go
+++ worker/processor.go
@@ -2,9 +2,11 @@ package worker
 
 import (
 	"context"
+	"fmt"
 
 	db "github.com/Drolfothesgnir/simplebank/db/sqlc"
 	"github.com/hibiken/asynq"
+	"github.com/rs/zerolog/log"
 )
 
 const (
@@ -22,12 +24,26 @@ type RedisTaskProcessor struct {
 	store  db.Store
 }
 
+func reportError(ctx context.Context, task *asynq.Task, err error) {
+	retried, _ := asynq.GetRetryCount(ctx)
+	maxRetry, _ := asynq.GetMaxRetry(ctx)
+	if retried >= maxRetry {
+		err = fmt.Errorf("retry exhausted for task %s: %w", task.Type(), err)
+	}
+	log.Error().Err(err).
+		Str("task", task.Type()).
+		Bytes("payload", task.Payload()).
+		Msg("task failed")
+}
+
 func NewRedisTaskProcessor(clientOpt asynq.RedisClientOpt, store db.Store) TaskProcessor {
 	server := asynq.NewServer(clientOpt, asynq.Config{
 		Queues: map[string]int{
 			QueueCritical: 10,
 			QueueDefault:  5,
 		},
+		ErrorHandler: asynq.ErrorHandlerFunc(reportError),
+		Logger:       NewLogger(),
 	})
 	return &RedisTaskProcessor{
 		server: server,
